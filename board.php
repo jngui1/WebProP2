7c56600dd2cf25a18737f950b70952a0f89d50cf
Brian <aladebrian@gmail.com>
2025-04-07 19:11:45 -0400

    changed filtered items and added random_questions
    added randomizer for for functiosn for gettin random categories.
    did all the other stuff i just said

--- board.php
+++ board.php
@@ -1,110 +1,123 @@
 <?php
-    session_start();
+require("randomizer.php");
+session_start();
+function create_board()
+{
+    $readin = getRandomCategories("filtered_csv.csv");
+    return $readin;
+}
+
 ?>
 <!DOCTYPE html>
 
 <html lang="en">
-    <head>
-        <title>Jeopardy - Board</title>
 
-        <meta charset="UTF-8">
+<head>
+    <title>Jeopardy - Board</title>
 
-        <link rel="stylesheet" type="text/css" href="layout.css">
-        
-    </head>
+    <meta charset="UTF-8">
 
-    <body>
-        <div id="turn_heading" class="heading"><h1>Player #'s turn!</h1></div>
+    <link rel="stylesheet" type="text/css" href="layout.css">
+
+</head>
+
+<body>
+    <div id="turn_heading" class="heading">
+        <h1>Player #'s turn!</h1>
+    </div>
+
+    <div id="p1_score_tally" class="score_tally">
+        <p>P2 Winnings: $0</p>
+    </div>
+
+    <div class="score_tally">
         
-        <div id="p1_score_tally" class="score_tally"><p>P1 Winnings: $0</p></div>
-        
-        <div id="p2_score_tally" class="score_tally"><p>P2 Winnings: $0</p></div>
-        
-        <div id="board_table">
-            <table>
-                <tr>
-                    <th>Category 1</th>
-                    
-                    <th>Category 2</th>
-                    
-                    <th>Category 3</th>
-                    
-                    <th>Category 4</th>
-                    
-                    <th>Category 5</th>
-                    
-                </tr>
-                
-                <tr>
-                    <td><a href="answer.php?cat=1&val=100">$100</a></td>
-                    
-                    <td><a href="answer.php?cat=1&val=200">$200</a></td>
-                    
-                    <td><a href="answer.php?cat=1&val=300">$300</a></td>
-                    
-                    <td><a href="answer.php?cat=1&val=400">$400</a></td>
-                    
-                    <td><a href="answer.php?cat=1&val=500">$500</a></td>
-                    
-                </tr>
-                
-                <tr>
-                    <td><a href="answer.php?cat=2&val=100">$100</a></td>
-                    
-                    <td><a href="answer.php?cat=2&val=200">$200</a></td>
-                    
-                    <td><a href="answer.php?cat=2&val=300">$300</a></td>
-                    
-                    <td><a href="answer.php?cat=2&val=400">$400</a></td>
-                    
-                    <td><a href="answer.php?cat=2&val=500">$500</a></td>
-                    
-                </tr>
-                
-                <tr>
-                    <td><a href="answer.php?cat=3&val=100">$100</a></td>
-                    
-                    <td><a href="answer.php?cat=3&val=200">$200</a></td>
-                    
-                    <td><a href="answer.php?cat=3&val=300">$300</a></td>
-                    
-                    <td><a href="answer.php?cat=3&val=400">$400</a></td>
-                    
-                    <td><a href="answer.php?cat=3&val=500">$500</a></td>
-                    
-                </tr>
-                
-                <tr>
-                    <td><a href="answer.php?cat=4&val=100">$100</a></td>
-                    
-                    <td><a href="answer.php?cat=4&val=200">$200</a></td>
-                    
-                    <td><a href="answer.php?cat=4&val=300">$300</a></td>
-                    
-                    <td><a href="answer.php?cat=4&val=400">$400</a></td>
-                    
-                    <td><a href="answer.php?cat=4&val=500">$500</a></td>
-                    
-                </tr>
-                
-                <tr>
-                    <tr>
-                    <td><a href="answer.php?cat=5&val=100">$100</a></td>
-                    
-                    <td><a href="answer.php?cat=5&val=200">$200</a></td>
-                    
-                    <td><a href="answer.php?cat=5&val=300">$300</a></td>
-                    
-                    <td><a href="answer.php?cat=5&val=400">$400</a></td>
-                    
-                    <td><a href="answer.php?cat=5&val=500">$500</a></td>
-                    
-                </tr>
-            
-            </table>
-        
-        </div>
-    
-    </body>
-    
+    </div>
+    <div id="board_table">
+        <table>
+            <tr>
+                <th>Category 1</th>
+
+                <th>Category 2</th>
+
+                <th>Category 3</th>
+
+                <th>Category 4</th>
+
+                <th>Category 5</th>
+
+            </tr>
+
+            <tr>
+                <td><a href="answer.php?cat=1&val=100">$100</a></td>
+
+                <td><a href="answer.php?cat=1&val=200">$200</a></td>
+
+                <td><a href="answer.php?cat=1&val=300">$300</a></td>
+
+                <td><a href="answer.php?cat=1&val=400">$400</a></td>
+
+                <td><a href="answer.php?cat=1&val=500">$500</a></td>
+
+            </tr>
+
+            <tr>
+                <td><a href="answer.php?cat=2&val=100">$100</a></td>
+
+                <td><a href="answer.php?cat=2&val=200">$200</a></td>
+
+                <td><a href="answer.php?cat=2&val=300">$300</a></td>
+
+                <td><a href="answer.php?cat=2&val=400">$400</a></td>
+
+                <td><a href="answer.php?cat=2&val=500">$500</a></td>
+
+            </tr>
+
+            <tr>
+                <td><a href="answer.php?cat=3&val=100">$100</a></td>
+
+                <td><a href="answer.php?cat=3&val=200">$200</a></td>
+
+                <td><a href="answer.php?cat=3&val=300">$300</a></td>
+
+                <td><a href="answer.php?cat=3&val=400">$400</a></td>
+
+                <td><a href="answer.php?cat=3&val=500">$500</a></td>
+
+            </tr>
+
+            <tr>
+                <td><a href="answer.php?cat=4&val=100">$100</a></td>
+
+                <td><a href="answer.php?cat=4&val=200">$200</a></td>
+
+                <td><a href="answer.php?cat=4&val=300">$300</a></td>
+
+                <td><a href="answer.php?cat=4&val=400">$400</a></td>
+
+                <td><a href="answer.php?cat=4&val=500">$500</a></td>
+
+            </tr>
+
+            <tr>
+            <tr>
+                <td><a href="answer.php?cat=5&val=100">$100</a></td>
+
+                <td><a href="answer.php?cat=5&val=200">$200</a></td>
+
+                <td><a href="answer.php?cat=5&val=300">$300</a></td>
+
+                <td><a href="answer.php?cat=5&val=400">$400</a></td>
+
+                <td><a href="answer.php?cat=5&val=500">$500</a></td>
+
+            </tr>
+
+        </table>
+
+    </div>
+
+</body>
+
 </html>
---
+++ randomizer.php
@@ -0,0 +1,44 @@
+<?php
+
+define("TOTAL_CATEGORY_COUNT", 18688);
+define("QUESTIONS_USED", 5);
+define("CATEGORIES_USED", 5);
+/**
+ * returns 5 categories. access the question and answer pairs through their values.
+ * @return list [category1 => [valueNumber => [answer => x, question => x], valueNumber => [answer => x, question => x] ...],
+ *           category2 => [valueNumber => [answer => x, question => x], ... ] ...]
+ */
+function getRandomCategories() {
+    $categories = [];
+    for ($i=0; $i < CATEGORIES_USED; $i++) { 
+        $categories[] = getRandomCategory();
+    }
+    print_r($categories);
+    return $categories;
+}
+    
+/** 
+ * @return [category => [valueNumber => [answer => x, question => x] times 5]]
+ * thats 1 category, with CATEGORIES_USED amount of value arrays containing the corresponding answer and question
+ * enough to fill a single column.
+ */
+function getRandomCategory()
+{
+    $file_name = "filtered_csv.csv";
+    $readin = file($file_name);
+    // $fields = str_getcsv($readin[0], separator:"|", escape:"\\");
+
+    $random_line = (rand(0, TOTAL_CATEGORY_COUNT) * 5) + 1;
+    $category = str_getcsv($readin[$random_line], separator:"|", escape:"\\")[1];
+    $categoryArray[$category] = array();
+    for ($i=$random_line; $i < 5 + $random_line; $i++) { 
+        $line = str_getcsv($readin[$i], separator:"|", escape:"\\");
+        $categoryArray[$category][$line[0]] = array("Answer" => $line[2], "Question" => $line[3]);
+    }
+    print_r($categoryArray);
+}
+// if (!isset($_COOKIE['CREATED_BOARD'])) {
+//     $readin = create_board();
+//     setcookie("CREATE_BOARD");
+// }
+getRandomCategories();
